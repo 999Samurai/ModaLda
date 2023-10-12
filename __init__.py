af3dad1f5347a8e87488189641bf56dfbd337a96
999diogo <999Samurai@protonmail.com>
2023-10-12 19:33:05 +0100

    finished flask integration and minimalist folder structure

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
-from .views.login import bp as login_bp
+from views.login import bp as bp_login
 
 app = Flask(__name__)
 
-app.register_blueprint(login_bp)
+app.register_blueprint(bp_login)
+
+if __name__ == '__main__':
+    app.run(debug=True)
